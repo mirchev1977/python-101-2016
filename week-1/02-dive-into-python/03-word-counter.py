c464229c56addd7da93fa4e9a8aaef581ac83244
Miroslav <mirchev1977@novihorizonti.eu>
2016-10-24 11:06:27 +0300

    word counter  - counted words by rows horizontally

--- week-1/02-dive-into-python/03-word-counter.py
+++ week-1/02-dive-into-python/03-word-counter.py
@@ -1,132 +1,53 @@
 # Word Counter
 def word_counter(inp):
-    word = inp[0]
-    inp.remove(word)
+    searched_word = inp[0]
+    inp.remove(searched_word)
     dimensions = inp[0]
     inp.remove(dimensions)
     dimensions = dimensions.split(" ")
-    dimensions = [int(x) for x in list(dimensions) if x != ' ']
-
+    dimensions = [int(x) for x in dimensions]
+    matrix_rows = dimensions[0]
+    matrix_cols = dimensions[1]
     total_ocurences = 0
 
-    # search horizontally
-    for row in range(int(dimensions[0])):
-        current = list(inp[row])
-        current = [x for x in current if x != ' ']
-        current = "".join(current)
-        occurences = current.count(word)
-        total_ocurences += occurences
+    for x in range(len(inp)):
+        val = inp[x].split(' ')
+        inp[x] = val
 
-        current = current[::-1]
-        occurences = current.count(word)
-        total_ocurences += occurences
-
-    # create list of char lists
-    list_of_char_lists = []
-    for row in inp:
-        row = [x for x in row if x != ' ']
-        list_of_char_lists.append(row)
-
-    # search vertically
-    for col in range(int(dimensions[1])):
-        current_word = ''
-        for row in range(dimensions[0]):
-            char = list_of_char_lists[row][col]
-            current_word += char
-        occurences = current_word.count(word)
-        total_ocurences += occurences
-        current_word = current_word[::-1]
-        occurences = current_word.count(word)
-        total_ocurences += occurences
-
-    # search left diagonal - along columns
-    row_length = dimensions[0]
-    for col in range(0, dimensions[1]):
-        current_word = ''
-        col_picker = col
-        for row in range(row_length):
-            char = list_of_char_lists[row][col_picker]
-            current_word += char
-            col_picker += 1
-        row_length -= 1
-        occurences = current_word.count(word)
-        total_ocurences += occurences
-        current_word = current_word[::-1]
-        occurences = current_word.count(word)
-        total_ocurences += occurences
-
-    # search left diagonal - along rows
-    col_end = dimensions[0] - 1
-    for row in range(1, dimensions[0]):
-        current_word = ''
-        row_pointer = row
-        for col in range(col_end):
-            char = list_of_char_lists[row_pointer][col]
-            current_word += char
-            row_pointer += 1
-        occurences = current_word.count(word)
-        total_ocurences += occurences
-        current_word = current_word[::-1]
-        occurences = current_word.count(word)
-        total_ocurences += occurences
-        col_end -= 1
-
-    # search right diagonal - along columns
-    row_end = 1
-    for col in range(dimensions[0] - 1):
-        current_word = ''
-        col_pointer = col
-        for row in range(row_end):
-            char = list_of_char_lists[row][col_pointer]
-            current_word += char
-            col_pointer -= 1
-        occurences = current_word.count(word)
-        total_ocurences += occurences
-        current_word = current_word[::-1]
-        occurences = current_word.count(word)
-        total_ocurences += occurences
-        row_end += 1
-
-    # search right diagonal - along rows
-    # col_end = dimensions[1]
-    # for row in range(1, dimensions[0]):
-    #     current_word = ''
-    #     col_pointer = dimensions[1] - 1
-    #     row_pointer = row
-    #     for col in range(col_end):
-    #         char = list_of_char_lists[row_pointer][col_pointer]
-    #         current_word += char
-    #         col_pointer -= 1
-    #         row_pointer += 1
-    #     occurences = current_word.count(word)
-    #     total_ocurences += occurences
-    #     current_word = current_word[::-1]
-    #     occurences = current_word.count(word)
-    #     total_ocurences += occurences
-    #     col_end -= 1
+    # iterate rows
+    for x in range(matrix_rows):
+        current_row = "".join(inp[x])
+        ocurrences = current_row.count(searched_word)
+        total_ocurences += ocurrences
+        current_row = current_row[::-1]
+        ocurrences = current_row.count(searched_word)
+        total_ocurences += ocurrences
+        print(current_row)
 
     print(total_ocurences)
 
 
-inp = ["ivan",
-       "5 4",
-       "i v a n",
-       "e v n h",
-       "i n a v",
-       "m v v n",
-       "q r i t"]
+"""
+# inp = ["ivan",
+#        "5 4",
+#        "i v a n",
+#        "e v n h",
+#        "i n a v",
+#        "m v v n",
+#        "q r i t"]
+"""
 
-# inp = [
-#     "actually",
-#     "8 15",
-#     "i v a n q h r e z g t z o y m",
-#     "e v n h t r x e k y d a i l c",
-#     "i a c t u a l l y m c x r l e",
-#     "m v c n p u a m n t l u e a a",
-#     "q r i t w e a q u p r x t u z",
-#     "p e a c t u a l l y w p y t m",
-#     "o y h t r e l u f p q n z c s",
-#     "p a c t u a l l y u r e q a r"
-# ]
+inp = [
+    "actually",
+    "8 15",
+    "i v a n q h r e z g t z o y m",
+    "e v n h t r x e k y d a i l c",
+    "i a c t u a l l y m c x r l e",
+    "m v c n p u a m n t l u e a a",
+    "q r i t w e a q u p r x t u z",
+    "p e a c t u a l l y w p y t m",
+    "o y h t r e l u f p q n z c s",
+    "p a c t u a l l y u r e q a r"
+]
 
 word_counter(inp)
