5ba239465a328643428b808a575fe2c1ba54da9e
Miroslav Mirchev <mirchev1977@novihorizonti.eu>
2016-10-26 18:58:26 +0300

    solved random numbers generator

---
+++ week-2/05-problems-with-files/02-random-integers/generate_numbers.py
@@ -0,0 +1,27 @@
+# generate_numbers.py
+import sys
+from random import randint
+import io
+
+
+def main(n):
+    n = int(n)
+    random_numbers = []
+    for x in range(n):
+        generated = randint(1, 1000)
+        random_numbers.append(str(generated))
+
+    concatinated_numbers = " ".join(random_numbers)
+
+    with io.FileIO("generated_numbers.txt", "w") as file:
+    	bytes = str.encode(concatinated_numbers)
+    	file.write(bytes)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+
+
+# import io
+
+# with io.FileIO("foobar.txt", "w") as file:
+#     file.write("Hello!")
