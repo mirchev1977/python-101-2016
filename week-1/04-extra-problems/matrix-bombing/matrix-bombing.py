ef39c6e5d6bafaccc61ade242d2fa633da75ab6d
Miroslav Mirchev <mirchev1977@novihorizonti.eu>
2016-10-26 12:12:36 +0300

    added some extra problems for week 1

---
+++ week-1/04-extra-problems/matrix-bombing/matrix-bombing.py
@@ -0,0 +1,15 @@
+# matrix bombing
+def matrix_bombing_plan(m):
+	pass
+
+
+input = ["10  10  10", "10   9  10", "10  10  10"]
+
+"""
+10  10  10
+10   9  10
+10  10  10
+"""
+
+
+matrix_bombing_plan(input)
