96ff3c9eff4dd64b8e52929a6785b08db1df5b2f
Miroslav Mirchev <mirchev1977@novihorizonti.eu>
2016-11-21 18:57:45 +0200

    added some more methods and tests

--- week-06/01-tree/Tree.py
+++ week-06/01-tree/Tree.py
@@ -6,7 +6,24 @@ class Tree:
         self.children = []
 
     def add_child(self, root, child):
-        self.children.append(child)
+        if self.root_is_current_tree(root):
+            subtree = Tree(child)
+            self.children.append(subtree)
+
+    def find_proper_node(self):
+        if len(self.children) <= 0:
+            return False
+
+    def has_children(self):
+        if len(self.children) > 0:
+            return True
+        return False
+
+    def root_is_current_tree(self, value):
+        if value == self.data:
+            return True
+        else:
+            return False
 
     def __str__(self):
         return str(self.data)
--- week-06/01-tree/Tree_test.py
+++ week-06/01-tree/Tree_test.py
@@ -8,20 +8,47 @@ class TreeTest(unittest.TestCase):
         self.tree = Tree(root=5)
 
     def test_tree_data(self):
+        print()
+        print("*** test_tree_data")
         print(self.tree.data)
         self.assertEqual(self.tree.data, 5)
+        print("***end test_tree_data")
+        print()
 
     def test_tree_string_representation(self):
         print(str(self.tree))
         self.assertEqual(str(self.tree), str(self.tree.data))
 
-
     def test_tree_children_exist(self):
+        print()
+        print("*** test test_tree_children_exist")
         self.tree.add_child(5, 4)
         self.tree.add_child(5, 7)
         self.tree.add_child(5, 9)
         print(self.tree.children)
         self.assertTrue(len(self.tree.children) > 0)
+        print("***end test test_tree_children_exist")
+        print()
+
+    def test_has_children(self):
+        print()
+        print("*** has_children")
+        self.tree.add_child(5, 4)
+        self.tree.add_child(5, 8)
+        self.tree.add_child(6, 9)
+        print(self.tree.has_children())
+        self.assertTrue(str(self.tree.children) == "[4, 8]")
+        print("***end has_children")
+        print()
+
+    def test_find_proper_node(self):
+        print()
+        print("*** test_find_proper_node")
+        print(self.tree.has_children())
+        self.tree.add_child(5, 19)
+        print(self.tree.has_children())
+        print("***end test_find_proper_node")
+        print()
 
 
 if __name__ == "__main__":
