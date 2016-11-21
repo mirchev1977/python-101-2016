d269f7750d6144217cd3f65499098a9d987b4385
Miroslav Mirchev <mirchev1977@novihorizonti.eu>
2016-11-21 23:59:31 +0200

    created method find and tested it

--- week-06/01-tree/Tree.py
+++ week-06/01-tree/Tree.py
@@ -9,10 +9,39 @@ class Tree:
         if self.root_is_current_tree(root):
             subtree = Tree(child)
             self.children.append(subtree)
+        else:
+            self.find_proper_node(root, child)
 
-    def find_proper_node(self):
+    def find(self, x):
+        if self.data == x:
+            return True
+        elif(len(self.children) > 0):
+            return self.__find_in_children(self.children, x)
+
+    def __find_in_children(self, children, x):
+        for tree in children:
+            if tree.data == x:
+                return True
+            else:
+                if len(tree.children) > 0:
+                    result = self.__find_in_children(tree.children, x)
+                    if result:
+                        return True
+
+        return False
+
+    def find_proper_node(self, root, child):
         if len(self.children) <= 0:
             return False
+        self.__find_node_by_value(self.children, root, child)
+
+    def __find_node_by_value(self, children, root, child):
+        for current_tree in children:
+            if current_tree.data == root:
+                current_tree.add_child(root, child)
+            else:
+                self.__find_node_by_value(
+                    current_tree.children, root, child)
 
     def has_children(self):
         if len(self.children) > 0:
@@ -30,4 +59,3 @@ class Tree:
 
     def __repr__(self):
         return self.__str__()
-
