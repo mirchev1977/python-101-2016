4eed31dcefac3d37cf1385a837f27d1deadccc29
Miroslav Mirchev <mirchev1977@novihorizonti.eu>
2016-12-16 18:58:57 +0200

    promoted user to doctor or patient

--- week-09/hospital-manager/main.py
+++ week-09/hospital-manager/main.py
@@ -1,4 +1,5 @@
 import sqlite3
+from random import choice
 from queries import *
 
 # connection
@@ -19,22 +20,42 @@ c.execute(CREATE_PATIENT_TABLE)
 c.execute(CREATE_HOSPITAL_STAY_TABLE)
 c.execute(CREATE_VISITATION_TABLE)
 
-# create a user
+
+def promote_to_doctor(baseuser):
+    academic_titles = ['general practicioner',
+                       'surgeon', 'anaestologist']
+    title = choice(academic_titles)
+
+    c.execute(PROMOTE_USER_TO_DOCTOR, (baseuser, title))
+    db.commit()
+
+
+def promote_to_patient(baseuser, doctor=None):
+    c.execute(PROMOTE_USER_TO_PATIENT, (baseuser))
+    db.commit()
+
+
+def decide_if_doctor(userId):
+    patient = c.execute(GET_USER_BY_ID, (userId,))
+    current = [x for x in patient]
+    middle = current[0]['USERNAME']
+    name = middle.split(' ')
+
+    if name[0] == 'dr.':
+        promote_to_doctor(str(current[0]['ID']))
+        return
+
+    promote_to_patient(str(current[0]['ID']))
+    return
 
 
 def create_user(username, password, age, is_active):
     c.execute(CREATE_NEW_USER,
               (username, password, age, is_active))
     db.commit()
+    lastId = c.lastrowid
+    decide_if_doctor(lastId)
 
 
-
-def promote_to_doctor(baseuser):
-    pass
-
-
-def promote_to_patient(baseuser, doctor=None):
-    pass
-
-
-create_user('dr. pesho', 'az_sym_pesho', 55, 0)
+create_user('dr. pesho', 'az_sym_dr_pesho', 55, 0)
+create_user('pesho', 'az_sym_pesho', 22, 0)
